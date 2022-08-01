ad73dad6f25e003bcb3e0244a4ff5a4697495f49
Arc-ad <barkovskiia@bk.ru>
2022-08-01 15:31:18 +0300

    Animation

--- src/main/java/sample/HomeController.java
+++ src/main/java/sample/HomeController.java
@@ -1,8 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 
 public class HomeController {
 
@@ -11,9 +18,32 @@ public class HomeController {
 
     @FXML
     private URL location;
+    @FXML
+    private Button butInSample;
+
 
     @FXML
     void initialize() {
+        butInSample.setOnAction(actionEvent -> {
+            System.out.println("asd");
+            openNewScene("/sample/sample.fxml");
+        });
+
+    }
+    public void openNewScene(String window)
+    {
+        butInSample.getScene().getWindow().hide();        //закрытие основного окна
+        FXMLLoader loader = new FXMLLoader();       //новое окно
+        loader.setLocation(getClass().getResource(window));      //передаем путь к этому окну
+        try {
+            loader.load();      //загружаем этот файл
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root = loader.getRoot();     //путь к файлу который нам нужно загрузить
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();        //показать и подождать пока окно отобразится
 
     }
 
